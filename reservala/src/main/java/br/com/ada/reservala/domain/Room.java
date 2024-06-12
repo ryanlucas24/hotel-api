441c6caab212facbebfa63936c7a63f5959f96e0
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-06-12 10:16:00 -0300

    Refactor: update of Room using lombok annotations to getter, setter, builder and args constructor

--- reservala/src/main/java/br/com/ada/reservala/domain/Room.java
+++ reservala/src/main/java/br/com/ada/reservala/domain/Room.java
@@ -1,51 +1,17 @@
 package br.com.ada.reservala.domain;
 
-public class Room {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class Room {
     private Integer roomNumber;
     private String type;
     private Integer price;
     private Boolean available;
-
-    public Room() {
-    }
-
-    public Room(Integer roomNumber, String type, Integer price, Boolean available) {
-        this.roomNumber = roomNumber;
-        this.type = type;
-        this.price = price;
-        this.available = available;
-    }
-
-    public Integer getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setRoomNumber(Integer roomNumber) {
-        this.roomNumber = roomNumber;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public Boolean getAvailable() {
-        return available;
-    }
-
-    public void setAvailable(Boolean available) {
-        this.available = available;
-    }
 }
