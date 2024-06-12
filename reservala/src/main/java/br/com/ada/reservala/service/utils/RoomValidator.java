baf2c1ccadb967083b65d47abe8e87cdaeb91f8a
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-06-12 09:35:15 -0300

    Feat: Creation of class RoomValidator to validate a room and his attributes

---
+++ reservala/src/main/java/br/com/ada/reservala/service/utils/RoomValidator.java
@@ -0,0 +1,43 @@
+package br.com.ada.reservala.service.utils;
+
+import br.com.ada.reservala.domain.Room;
+import br.com.ada.reservala.utils.Notification;
+import br.com.ada.reservala.utils.Validator;
+
+public class RoomValidator implements Validator<Room> {
+
+    @Override
+    public boolean isNull(Room room) {
+        return room == null;
+    }
+
+    @Override
+    public Notification validate(Room room) {
+        Notification notification = new Notification();
+        if(isNull(room)){
+            notification.addError("Room is null!");
+            return notification;
+        }
+        if(hasNullAttributes(room, notification).hasErrors())
+            return notification;
+        if(room.getRoomNumber() <= 0)
+            notification.addError("Room number negative or equals zero!");
+        if(room.getPrice() <= 0)
+            notification.addError("Room price negative or equals zero!");
+        if(room.getType().isBlank())
+            notification.addError("Room type is blank!");
+        return notification;
+    }
+
+    private Notification hasNullAttributes(Room room, Notification notification) {
+        if (room.getRoomNumber() == null)
+            notification.addError("Room number is null!");
+        if (room.getPrice() == null)
+            notification.addError("Room price is null!");
+        if (room.getType() == null)
+            notification.addError("Room type is null!");
+        if (room.getAvailable() == null)
+            notification.addError("Room available is null!");
+        return notification;
+    }
+}
